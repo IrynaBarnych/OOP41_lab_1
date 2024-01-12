b444f2b2f063954df5de8548d2b142f2cb866762
IrynaBarnych <iryna.barnych@gmail.com>
2024-01-13 01:17:12 +0200

    Завдання 5 Показати міста студентів. Назви міст мають бути унікальними.

--- lab1.sql
+++ lab1.sql
@@ -1,6 +1,6 @@
 --Завдання 5
--- Показати ПІБ усіх студентів з мінімальною оцінкою,
---більшою, ніж зазначена.
-SELECT student_name FROM Student_Grades
-WHERE min_grade < 8
+-- Показати країни студентів. Назви країн мають бути
+--унікальними.
+SELECT DISTINCT country FROM Student_Grades
+
 
