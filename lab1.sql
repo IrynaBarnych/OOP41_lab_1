0d6a18eda84de11f0297d129334bee51a21bd186
IrynaBarnych <iryna.barnych@gmail.com>
2024-01-13 01:31:19 +0200

    Завдання 5 Показати назви усіх предметів із мінімальними середніми оцінками. Назви предметів мають бути унікальними.

--- lab1.sql
+++ lab1.sql
@@ -1,5 +1,6 @@
 --Завдання 5
--- Показати назви груп. Назви груп мають бути унікальними.
-SELECT DISTINCT group_name FROM Student_Grades
+-- Показати назви усіх предметів із мінімальними середніми оцінками. Назви предметів мають бути унікальними.
+SELECT DISTINCT min_subject_name FROM Student_Grades
+WHERE average_grade_per_year = (SELECT MIN(average_grade_per_year)FROM Student_Grades)
 
 
